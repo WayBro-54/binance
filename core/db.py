f37ce5346ac49e1731d0dc8961266899241cec19
waybro <a.ultanov@bk.ru>
2023-03-26 16:43:50 +0700

    p1

--- core/db.py
+++ core/db.py
@@ -3,19 +3,22 @@ from sqlalchemy.orm import declarative_base, sessionmaker, declared_attr
 
 from .config import DB_NAME, DB_ROOT
 
-Base = declarative_base()
-
 class PreBase:
     @declared_attr
     def __tablename__(cls):
         return cls.__name__.lower()
 
 
-async_engine = create_async_engine(DB_ROOT + DB_NAME)
-async_session = sessionmaker(async_engine, class_=AsyncSession)
+Base = declarative_base(cls=PreBase)
+# DB_ROOT = 'sqlite+aiosqlite:///./'
+# DB_NAME = 'binance.db'
+
+engine = create_async_engine('sqlite+aiosqlite:///./binance.db')
+AsyncSessionLocal = sessionmaker(engine, class_=AsyncSession)
 
 
 async def create_database():
-    async with async_engine.begin() as conn:
-        async with async_session() as session:
-            await conn.run_sync(Base.metadata.create_all)
+    async with AsyncSessionLocal() as session:
+        yield session
+        # async with async_session() as session:
+        #     await conn.run_sync(Base.metadata.create_all)
--- main.py
+++ main.py
@@ -1,57 +1,195 @@
 import asyncio
 import os
-from pprint import pprint
+from datetime import datetime, timedelta
+from decimal import Decimal
+
+import talib
 import numpy as np
-from core.config import API_KEY, SECRET_KEY, DB_NAME
-from core.db import create_database, async_session
-from core.table import InfoEthusdt
 from binance import AsyncClient, BinanceSocketManager
-from binance.exceptions import BinanceAPIException
+from sqlalchemy import select
+
+from core.config import (API_KEY, SECRET_KEY, DB_NAME, EVERY_5MINUTES,
+                         EVERY_15MINUTES, CLOSE, HIGH, LOW, INDEX_FOR_1MIN,
+                         INDEX_FOR_5MIN, INDEX_FOR_15MIN)
+from core.db import create_database, AsyncSessionLocal
+from core.table import InfoEthusdt
 
 
-async def get_stoch():
-    client = await AsyncClient.create(api_key=API_KEY, api_secret=SECRET_KEY)
-
-    try:
-        klines = await client.get_klines(symbol='ETHUSDT', interval=client.KLINE_INTERVAL_1MINUTE, limit=14)
-        closes = np.array([float(x[4]) for x in klines])
-        highs = np.array([float(x[2]) for x in klines])
-        lows = np.array([float(x[3]) for x in klines])
-        highest_high = np.max(highs[:-1])
-        lowest_low = np.min(lows[:-1])
-        k_value = ((closes[-1] - lowest_low) / (highest_high - lowest_low)) * 100
-        d_value = np.mean(k_value[-3:])
-        # return k_value, d_value
-        print(d_value, k_value)
-    except BinanceAPIException as e:
-        print(e)
-    except Exception as e:
-        print(e)
-    finally:
-        await client.close_connection()
+async def percent_price_one_hour(session, timestamp, price):
+    """Расчитывем процент изменения цена за 1 час."""
+    timestamp = timestamp-3600
+    price_old = await session.execute(
+        select(InfoEthusdt.price).where(InfoEthusdt.timestamp == timestamp)
+    )
+    price_old = price_old.scalar()
+    print(price_old)
+    print(price)
+    if price_old:
+        price_old = price_old
+        percent = abs((Decimal(price) - price_old) / price_old * 100)
+        if percent >= 1:
+            print(f'Цена изменилась на {percent:.2f} процентов.')
 
 
-# async def get_ethusdt_ticker(session):
-#     client = await AsyncClient.create(api_key=API_KEY, api_secret=SECRET_KEY)
-#     bm = BinanceSocketManager(client)
-#     ts = bm.trade_socket('ETHUSDT')
-#     async with ts as tscm:
-#         while True:
-#             res = await tscm.recv()
-#             ticker_data = {
-#                 'price': res['p'],
-#                 'timestamp': res['T'],
-#             }
-#             tecker = InfoEthusdt(**ticker_data)
-#             session.add(tecker)
-#             await session.commit()
-#             await client.close_connection()
+async def get_now100_bars(data_bars100, client, current_time):
+    """Получаем новые данные о 100 новых баров."""
+
+    if current_time.second == 0:
+        # получаем данные 100 баров 1м т.ф.
+        new_data_m1 = await client.futures_klines(symbol='ETHUSDT',
+                                                  interval=client.KLINE_INTERVAL_1MINUTE,
+                                                  limit=100),
+        data_bars100[INDEX_FOR_1MIN] = new_data_m1[0]
+
+    if (current_time.minute in EVERY_5MINUTES) and (current_time.second == 0):
+        # получаем данные 100 баров 5м т.ф.
+        new_data_m5 = await client.futures_klines(symbol='ETHUSDT',
+                                                  interval=client.KLINE_INTERVAL_5MINUTE,
+                                                  limit=100),
+        data_bars100[INDEX_FOR_5MIN] = new_data_m5[0]
+
+    if (current_time.minute in EVERY_15MINUTES) and (current_time.second == 0):
+        # получаем данные 100 баров 15м т.ф.
+        new_data_m15 = await client.futures_klines(symbol='ETHUSDT',
+                                                   interval=client.KLINE_INTERVAL_15MINUTE,
+                                                   limit=100),
+        data_bars100[INDEX_FOR_15MIN] = new_data_m15[0]
+
+    return data_bars100
+
+
+async def get_stoch_now(klines):
+    """Функция получения текущих данных стохастика."""
+    closes = np.array([float(x[4]) for x in klines])
+    highs = np.array([float(x[2]) for x in klines])
+    lows = np.array([float(x[3]) for x in klines])
+    fastk, fastd = talib.STOCH(
+        close=closes,
+        high=highs,
+        low=lows,
+        fastk_period=14,
+        slowk_period=1,
+        slowd_period=3
+    )
+    return fastk[-1], fastd[-1]
+
+
+async def analysis_stoch(data):
+    """Функция анализа данных стохастика."""
+    # получаем значение стохастика
+    res = await asyncio.gather(
+        get_stoch_now(data[INDEX_FOR_1MIN]),
+        get_stoch_now(data[INDEX_FOR_5MIN]),
+        get_stoch_now(data[INDEX_FOR_15MIN]),
+    )
+
+    # присваиваем переменные значениям стохастика
+    m1_k, m1_d = res[INDEX_FOR_1MIN]
+    m5_k, m5_d = res[INDEX_FOR_5MIN]
+    m15_k, m15_d = res[INDEX_FOR_15MIN]
+
+    if (m1_k >= m1_d >= 85) and (m5_k and m5_d >= 65) and (m15_k >= m15_d >= 85):
+        print('Рассматриваем вход в sell')
+
+    if (m1_k <= m1_d <= 15) and (m5_k and m5_d <= 35) and (m15_k <= m15_d <= 15):
+        print('Рассматриваем вход в buy')
+
+
+async def get_kline_(bm, bars):
+    """Функция обновляет значение текущего бара."""
+    async with bm as bm_:
+        res = await bm_.recv()
+        bars[-1][HIGH] = res['k']['h']
+        bars[-1][CLOSE] = res['k']['c']
+        bars[-1][LOW] = res['k']['l']
+        return bars
+
+
+async def analysis_eth(bm, client):
+    # объявляем сокеты для получения данных
+    tsm1 = bm.kline_futures_socket('ETHUSDT', client.KLINE_INTERVAL_1MINUTE)
+    tsm5 = bm.kline_futures_socket('ETHUSDT', client.KLINE_INTERVAL_5MINUTE)
+    tsm15 = bm.kline_futures_socket('ETHUSDT', client.KLINE_INTERVAL_15MINUTE)
+
+    # загружаем историю
+    get_100bars = await asyncio.gather(
+        client.futures_klines(symbol='ETHUSDT', interval=client.KLINE_INTERVAL_1MINUTE, limit=100),
+        client.futures_klines(symbol='ETHUSDT', interval=client.KLINE_INTERVAL_5MINUTE, limit=100),
+        client.futures_klines(symbol='ETHUSDT', interval=client.KLINE_INTERVAL_15MINUTE, limit=100)
+    )
+    while True:
+        # обновляем 100 полседних баров
+        # знаю, что это не камельфо...
+        curr_date = datetime.now()
+        get_100bars = await get_now100_bars(get_100bars, client, curr_date)
+
+        # обновляем данные таймфреймов для последнего бара
+        res = await asyncio.gather(
+            get_kline_(tsm1, get_100bars[INDEX_FOR_1MIN]),
+            get_kline_(tsm5, get_100bars[INDEX_FOR_5MIN]),
+            get_kline_(tsm15, get_100bars[INDEX_FOR_15MIN])
+        )
+
+        # проводим анализ стохастика
+        await analysis_stoch(res)
+
+
+async def get_history_price(client, session):
+    end_time = datetime.utcnow()
+    start_time = end_time - timedelta(hours=1)
+    start_str = start_time.strftime('%Y-%m-%d %H:%M:%S')
+
+    klines = await client.futures_historical_klines(symbol='ETHUSDT',
+                                                    interval=client.KLINE_INTERVAL_1MINUTE,
+                                                    start_str=start_str,
+                                                    limit=60)
+    for i in klines:
+        price = i[1]
+        timestamp = int(str(i[0])[:-3])
+
+        data_ticker = {
+            'price': price,
+            'timestamp': timestamp
+        }
+        ticker = InfoEthusdt(**data_ticker)
+        session.add(ticker)
+        await session.commit()
+
+
+async def get_price_eth(bm, session, client):
+
+    await get_history_price(client, session)
+    ts_p = bm.kline_futures_socket('ETHUSDT', client.KLINE_INTERVAL_1MINUTE)
+    async with ts_p as tsbm:
+        while True:
+            res = await tsbm.recv()
+
+            price = res['k']['c']
+            timestamp = int(str(res['k']['t'])[:-3])
+            data_ticker = {
+                'price': price,
+                'timestamp': timestamp
+            }
+
+            is_exists = await session.execute(
+                select(InfoEthusdt.id).where(InfoEthusdt.timestamp == timestamp).limit(1)
+            )
+            is_exists_id = is_exists.scalar()
+            if not is_exists_id:
+                ticker = InfoEthusdt(**data_ticker)
+                session.add(ticker)
+                await session.commit()
+            await percent_price_one_hour(session, timestamp, price)
 
 
 async def main():
-    async with async_session() as session:
-        # await get_ethusdt_ticker(session)
-        await get_stoch()
+    client = await AsyncClient.create(api_key=API_KEY, api_secret=SECRET_KEY)
+    bm = BinanceSocketManager(client)
+    async with AsyncSessionLocal() as session:
+        await asyncio.gather(
+            # analysis_eth(bm, client),
+            get_price_eth(bm, session, client)
+        )
 
 
 if __name__ == '__main__':
